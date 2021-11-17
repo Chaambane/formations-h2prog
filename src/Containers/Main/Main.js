a2c0cd6ceeaf87833d44e4cb1fd82e270b305b28
FAIDINE <mohamed.faidine@live.fr>
2021-11-17 15:05:45 +0100

    feat(bouton) ajout des boutons pour filtrer les formations

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -1,13 +1,15 @@
 import {Container} from 'react-bootstrap';
 import TitreH1 from '../../Components/TitreH1/TitreH1';
+import Bouton from '../Boutton/Boutton';
 
 function Main(){
     return(
         <>
             <TitreH1>Les formations <span className="text-info">H2PROG</span></TitreH1>
             <Container>
-                <div>Titre</div>
-                <div>Boutton</div>
+                <Bouton>PHP</Bouton>
+                <Bouton>Javascript</Bouton>
+                <Bouton>Algorithmique</Bouton>
                 <div>Table</div>
             </Container>
         </>
