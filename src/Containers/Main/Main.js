c3580e9b4b16277ab38424c66482f7a845887302
FAIDINE <mohamed.faidine@live.fr>
2021-11-17 14:56:17 +0100

    premier commit

---
+++ src/Containers/Main/Main.js
@@ -0,0 +1,14 @@
+import {Container} from 'react-bootstrap';
+function Main(){
+    return(
+        <>
+            <Container>
+                <div>Titre</div>
+                <div>Boutton</div>
+                <div>Table</div>
+            </Container>
+        </>
+    )
+};
+
+export default Main;
